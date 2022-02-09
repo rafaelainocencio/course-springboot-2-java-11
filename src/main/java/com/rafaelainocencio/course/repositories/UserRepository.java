564ce97b4dfd93b0eb9ea44492682daea228af06
rafaelainocencio <rafaelainocencioo@gmail.com>
2022-02-09 16:33:34 -0300

    Service layer, component registration

--- src/main/java/com/rafaelainocencio/course/repositories/UserRepository.java
+++ src/main/java/com/rafaelainocencio/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rafaelainocencio.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>   {
 	
 }
