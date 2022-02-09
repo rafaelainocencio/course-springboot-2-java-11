97a5fb7e8d9102be1278e3e6eb7fc7fb00147163
rafaelainocencio <rafaelainocencioo@gmail.com>
2022-02-09 15:53:26 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/rafaelainocencio/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelainocencio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaelainocencio.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>   {
+	
+}
